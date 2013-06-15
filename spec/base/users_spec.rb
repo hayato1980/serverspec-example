070a73999f0a21b552090cab04a847ca9a4011ba
Hayato ITO <haya10.ito@gmail.com>
2013-06-15 21:00:36 +0900

    refactor users_spec

--- spec/base/users_spec.rb
+++ spec/base/users_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
-describe user('root') do
-  it { should exist }
-  it { should belong_to_group 'root' }
-  it { should have_uid 0 }
-  it { should have_home_directory 'root' }
-  it { should have_login_shell '/bin/bash' }
-  it { should have_authorized_key '' }
+users = [
+  { :user => 'root', :group => 'root' ,:uid => 0, :home => '/root' , :loginshell => '/bin/bash' , :authorized_key => ''}
+]
+
+users.each do |u|
+  describe user(u[:user]) do
+    it { should exist }
+    it { should belong_to_group u[:group] }
+    it { should have_uid u[:uid] }
+    it { should have_home_directory u[:home] }
+    it { should have_login_shell u[:loginshell] }
+    it { should have_authorized_key u[:authorized_key] } unless u[:authorized_key].nil?
+  end
 end
 
-describe group('root') do
-  it { should exist }
-  it { should have_gid 0 }
+groups = [
+  { :group => 'root', :gid => 0 }
+]
+
+groups.each do |g|
+  describe group(g[:group]) do
+    it { should exist }
+    it { should have_gid g[:gid] }
+  end
 end
