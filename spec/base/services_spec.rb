68477ccecadcc8a80ad223da41a63c7bc5675cbc
Hayato ITO <haya10.ito@gmail.com>
2013-06-22 12:01:24 +0900

    a bit fix

--- spec/base/services_spec.rb
+++ spec/base/services_spec.rb
@@ -5,14 +5,14 @@ disable_services = ['postfix']
 
 enable_services.each do |s|
   describe service(s) do 
-    it { should be_enabled   };
+    it { should be_enabled   }
     it { should be_running   } 
   end
 end
 
 disable_services.each do |s|
   describe service(s) do 
-    it { should_not be_enabled   };
+    it { should_not be_enabled   }
     it { should_not be_running   } 
   end
 end
